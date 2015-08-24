52f52448605e5bb2112b937a2dfc896b1ecb55a2
Doran <samanthadoran3@gmail.com>
2015-08-24 01:03:32 -0400

    Add comments to explain everything

--- field.nim
+++ field.nim
@@ -3,27 +3,47 @@ type
   Field* = seq[seq[State]]
 
 proc sumNine(x: int, y: int, field: Field): int =
+  #The state of a cell can be described by the sum of itself and its neigbours
+
+  #Loop through the rows surrounding it
   for yIndex in y-1..y+1:
+    #Don't access bad indices
     if yIndex < 0 or yIndex >= len(field):
       continue
+
+    #Loop through each cell in each row
     for xIndex in x-1..x+1:
+      #Don't access bad indices
       if xIndex < 0 or xIndex >= len(field[yIndex]):
         continue
+
+      #Add 1 to result if the cell is alive, else 0
       result += (if field[yIndex][xIndex].current: 1 else: 0)
 
 proc logic*(field: var Field) =
+  #Calculate what the field will look like next
   for y in 0..<len(field):
     for x in 0..<len(field[y]):
+
+      #The future state of a cell can be described by the sum of itself
+      #and its neighbours
       var sum: int = sumNine(x, y, field)
       case sum
+
+      #If the sum is three, the cell will always live through
       of 3:
         field[y][x].next = true
+
+      #If the sum is four, it will retain state
       of 4:
         field[y][x].next = field[y][x].current
+
+      #Otherwise, it dies of some cause
       else:
         field[y][x].next = false
 
 proc step*(field: var Field) =
+  #Move the next state into the current state
   for y in 0..<len(field):
     for x in 0..<len(field[y]):
       field[y][x].current = field[y][x].next
--- main.nim
+++ main.nim
@@ -11,17 +11,24 @@ proc main() =
   for i in 0..<len(field):
     field[i] = newSeq[State](height)
 
+  #A quick blinker to start us off
   field[1][2].current = true
   field[2][2].current = true
   field[3][2].current = true
 
+  #Go through 5 generations
   for i in 0..5:
+    #Figure out how the field will look on the next iteration
     field.logic()
+
+    #Print the field
     for row in field:
       for cell in row:
         write(stdout, if cell.current: $1 else: $0)
       echo()
     echo("---------")
+
+    #Move to the next generation
     field.step()
 
 main()
